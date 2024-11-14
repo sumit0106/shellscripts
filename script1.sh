54fa239756b74f9d6ca1cf77364e46aa806d32d9
root <root@ip-172-31-8-65.ap-south-1.compute.internal>
2024-11-14 03:31:09 +0000

    This is EC2 test

--- script1.sh
+++ script1.sh
@@ -2,4 +2,4 @@ echo "We are trying this practical from linux EC2"
 echo "We are from batch-25 and learning git/gihub"
 echo "Hi Good Morning!"
 echo "This is second commit file."
-
+echo "This edit is for EC2 checking."
